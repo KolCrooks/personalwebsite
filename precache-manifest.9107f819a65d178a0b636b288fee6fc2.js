78aadc72f8c45dd7eb92c6c5eb9a84c706a3b7bb
Kol Crooks <kolcrooks10@gmail.com>
2020-01-23 20:21:09 -0500

    Updates

---
+++ precache-manifest.9107f819a65d178a0b636b288fee6fc2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca291c1047b849518674f0b9bb1e3950",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b5d2875019bbb545d957",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "3d4b0948d87961f6815e",
+    "url": "/static/css/main.84922342.chunk.css"
+  },
+  {
+    "revision": "b5d2875019bbb545d957",
+    "url": "/static/js/2.da918e4e.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.da918e4e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3d4b0948d87961f6815e",
+    "url": "/static/js/main.07c38342.chunk.js"
+  },
+  {
+    "revision": "b36d1a0ba5664ebbb9fd",
+    "url": "/static/js/runtime-main.b4d6441b.js"
+  }
+]);
